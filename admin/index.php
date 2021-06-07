c9a53c9209ff93d81c64d53179c2dc6f41ccfc44
Adria Tisnawati Aprilia <adriatisnawatiaprilian@gmail.com>
2021-06-07 23:10:16 +0700

    buat index admin

---
+++ admin/index.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<title>Administrator</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+
+	<input type="checkbox" id="nav-toggle">
+	<div class="sidebar">
+		<div class="sidebar-brand">
+			<h2><span class="fa fa-edit"></span> <span>Bearbucks</span> </h2>
+		</div>
+
+		<div class="sidebar-menu">
+			<ul>
+				<li>
+					<a href="" class="active">
+						<span class="fa fa-dashboard"></span>
+						<span> Home </span>
+					</a>
+				</li>
+				<li>
+					<a href="">
+						<span class="fa fa-shopping-cart"></span>
+						<span> Produk </span>
+					</a>
+				</li>
+				<li>
+					<a href="">
+						<span class="fa fa-money"></span>
+						<span> Pembayaran </span>
+					</a>
+				</li>
+				<li>
+					<a href="">
+						<span class="fa fa-users"></span>
+						<span> Users </span>
+					</a>
+				</li>
+				<li>
+					<a href="">
+						<span class="fa fa-sign-out"></span>
+						<span> Log Out </span>
+					</a>
+				</li>
+			</ul>
+		</div>
+	</div>	
+
+	<div class="main-content">
+		<header>
+			<h1>
+				<label for="nav-toggle">
+					<span class="fa fa-bars"></span>
+				</label>
+
+				.:: Aloha ::.
+			</h1>
+
+			<div class="search-wrapper">
+				<span class="fa fa-search"></span>
+				<input type="search" placeholder="Search Here" name="">
+			</div>
+
+			<div class="user-wrapper">
+				<img src="img/gambar.png" width="30px" height="30px" alt="">
+				<div>
+					<h4>Admin</h4>
+					<small>Super Admin</small>
+				</div>
+			</div>
+
+		</header>
+
+		<main>
+			<div class="cards">
+				<div class="card-single">
+					<div>
+						<h1>54</h1>
+						<span>Customers</span>
+					</div>
+					<div class="">
+						<span class="fa fa-dashboard"></span>
+					</div>
+				</div>
+
+				<div class="card-single">
+					<div>
+						<h1>54</h1>
+						<span>Customers</span>
+					</div>
+					<div class="">
+						<span class="fa fa-dashboard"></span>
+					</div>
+				</div>
+				<div class="card-single">
+					<div>
+						<h1>54</h1>
+						<span>Customers</span>
+					</div>
+					<div class="">
+						<span class="fa fa-dashboard"></span>
+					</div>
+				</div>
+				<div class="card-single">
+					<div>
+						<h1>54</h1>
+						<span>Customers</span>
+					</div>
+					<div class="">
+						<span class="fa fa-dashboard"></span>
+					</div>
+				</div>
+				<div class="card-single">
+					<div>
+						<h1>54</h1>
+						<span>Customers</span>
+					</div>
+					<div class="">
+						<span class="fa fa-dashboard"></span>
+					</div>
+				</div>
+
+				<div class="card-single">
+					<div>
+						<h1>54</h1>
+						<span>Customers</span>
+					</div>
+					<div class="">
+						<span class="fa fa-dashboard"></span>
+					</div>
+				</div>
+
+				<div class="card-single">
+					<div>
+						<h1>54</h1>
+						<span>Customers</span>
+					</div>
+					<div class="">
+						<span class="fa fa-dashboard"></span>
+					</div>
+				</div>
+
+				<div class="card-single">
+					<div>
+						<h1>$54</h1>
+						<span>Customers</span>
+					</div>
+					<div class="">
+						<span class="fa fa-dashboard"></span>
+					</div>
+				</div>
+			</div>
+
+			<div class="recent-grid">
+				<div class="projects">
+					<div class="card">
+						<div class="card-header">
+							<h3>Recent Projects</h3>
+
+							<button>
+								See All
+								<span class="fa fa-arrow-right"></span>
+							</button>
+						</div>
+
+						<div class="card-body">
+							<div class="table-responsive">
+								
+								<table width="100%">
+									<thead>
+										<tr>
+											<td>Project Title</td>
+											<td>Departement</td>
+											<td>Status</td>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
+											<td>UI / UX Design</td>
+											<td>UI Team</td>
+											<td>
+												<span class="status orange"></span> Review
+											</td>
+										</tr>
+										<tr>
+											<td>Web Development</td>
+											<td>UI Team</td>
+											<td>
+												<span class="status pink"></span> Pending
+											</td>
+										</tr>
+										<tr>
+											<td>Web Admin</td>
+											<td>UI Team</td>
+											<td>
+												<span class="status purple"></span> Link
+											</td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="customers">
+					<div class="card">
+						<div class="card-header">
+							<h3>News Customer</h3>
+
+							<button>
+								See All
+								<span class="fa fa-arrow-right"></span>
+							</button>
+						</div>
+
+						<div class="card-body">
+							<div class="customer">
+								<div class="info">
+									<img src="img/gambar.png" width="40px" height="40px" alt="">
+									<div>
+										<h4>Lewis S. Cunni</h4>
+										<small>CEO Expert</small>
+									</div>
+								</div>
+								<div class="contact">
+									<span class="fa fa-dashboard"></span>
+									<span class="fa fa-dashboard"></span>
+									<span class="fa fa-dashboard"></span>
+								</div>
+							</div>
+							<div class="customer"></div>
+							<div class="customer">
+								<div class="info">
+									<img src="img/gambar.png" width="40px" height="40px" alt="">
+									<div>
+										<h4>Lewis S. Cunni</h4>
+										<small>CEO Expert</small>
+									</div>
+								</div>
+								<div class="contact">
+									<span class="fa fa-dashboard"></span>
+									<span class="fa fa-dashboard"></span>
+									<span class="fa fa-dashboard"></span>
+								</div>
+							</div>
+							<div class="customer">
+								<div class="info">
+									<img src="img/gambar.png" width="40px" height="40px" alt="">
+									<div>
+										<h4>Lewis S. Cunni</h4>
+										<small>CEO Expert</small>
+									</div>
+								</div>
+								<div class="contact">
+									<span class="fa fa-dashboard"></span>
+									<span class="fa fa-dashboard"></span>
+									<span class="fa fa-dashboard"></span>
+								</div>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+
+		</main>
+	</div>
+
+</body>
+</html>
